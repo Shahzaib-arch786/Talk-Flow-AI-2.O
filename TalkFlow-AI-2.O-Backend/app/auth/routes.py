7d5666cdd2f981398e1c23b5ee5d711f51f3e4f8
Muhammad Ali Shahzaib <shahzaib02122001@gmail.com>
2026-02-16 22:35:48 +0500

    protected routes added in backend

--- TalkFlow-AI-2.O-Backend/app/auth/routes.py
+++ TalkFlow-AI-2.O-Backend/app/auth/routes.py
@@ -67,7 +67,8 @@ def login(
 
     return {
         "access_token": access_token,
-        "token_type": "bearer"
+        "token_type": "bearer", 
+        "role": user.role
     }
 
 # ---------------- PROTECTED ROUTE ----------------
