97b0bcbea60c5f2894e41f53f469e04010e24c73
Muhammad Ali Shahzaib <shahzaib02122001@gmail.com>
2026-04-25 23:02:28 +0500

    deploy changes

--- TalkFlow-AI-2.O-Backend/app/demo_ai/stt/openai_whisper.py
+++ TalkFlow-AI-2.O-Backend/app/demo_ai/stt/openai_whisper.py
@@ -8,6 +8,7 @@ class OpenAIWhisper:
         with open(audio_path, "rb") as audio:
             response = openai.audio.transcriptions.create(
                 model="whisper-1",
-                file=audio
+                file=audio,
+                language="ur",
             )
         return response.text
